9b7b533ba3bb90a057a3b2e6287365ef30d1ae26
suriyaweb <suris3631@gmail.com>
2024-10-09 23:46:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,223 @@
+// console.log("ghgg");
+//  var
+
+// variable declaration
+// var string = "suriya"; 
+// /variable redeclaration
+// var strin1 ="kumar";
+//   strin1 ="fdhffgfgfgffgg";
+
+//   reassignment
+// console.log(string,strin1)
+
+
+// let
+// let name ="ghgg";
+//  name = 123454666;
+//  console.log(name);
+
+//  const str = "hufhg"
+//  str ="hjhhj";
+//  console.log(str)
+//  var name="suriya";
+//  var name="suriya";
+//  let age="23";
+//   age="24";
+//  console.log(age);
+// let n = 'hghguhgufgyufyuy';
+// console.log(typeof n);
+
+
+// let data = suriya,madurai,tn;
+// console.log(data);
+// let data  =["name","age","city","state"]
+// console.log(data)
+// let person= {
+//     name :"suriya",
+//     age :"age",
+//     mark :"80",
+//     city :"madurai",
+// }
+// console.log(person)
+// function addNumbers(a, b) {
+//     return a + b;
+// }
+
+// let result = addNumbers(5, 10);
+// console.log(result);  // Output: 15
+
+// function addNumbers(a,b) {
+//     return a + b;
+// }
+// var submit = addNumbers(20,30);
+// console.log(submit);
+// function 
+// let x=5;
+// let y=5;
+// // addition
+//  console.log(x+y);
+//  console.log(x-y);
+//  console.log(x*y);
+//  console.log(x%y);
+//  console.log(x/y);
+//  console.log(x++);
+//   console.log(x++);
+//   console.log(y--);
+// console.log(y--);
+// assigment operter
+// let x='10';
+// console.log(typeof x)
+// console.log(x=10)
+// console.log(x+=5)
+// console.log(x-=3)
+// console.log(x*=2)
+// let number = 20;
+let number =0;
+    if (number < 0){
+         console.log ( "The number is positive.");
+    }
+    else if (number < 0){
+        console.log( "The number is negative.");
+    }
+   else(number < 0);{
+       console.log( "The number is zero.");
+    }
+let day =1
+switch(day =3){
+    case 1:
+    console.log("monday");
+    break;
+    case 2:
+    console.log("tuseday");
+    break;
+    case 3:
+    console.log("wednesday");
+    break;
+    case4 :
+    console.log("thuseday");
+    break;
+    case 5:
+    console.log("friday");
+    break;
+    case 6:
+    console.log("saturday");
+    break;
+    case 7:
+    console.log("sunday");
+    break;
+}
+
+
+ 
+
+
+
+
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
